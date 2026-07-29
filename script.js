0df7022b13c84efa411a1f88fedc8bd5ee1e09e5
Govind Sharma <150214417+govindsharma56@users.noreply.github.com>
2026-07-29 17:45:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here!
+let infinite=document.querySelector('#infi-list');
+let count=0
+ for(let i=1;i<=10;i++){
+	 let li=document.createElement('li');
+	   li.textContent=`Item ${i}`
+	 infinite.appendChild(li);
+	 count=i+1;
+}
+infinite.addEventListener('scroll',()=>{
+	  if (infinite.scrollTop + infinite.clientHeight >=
+    infinite.scrollHeight - 5
+  )
+	  {
+		   for (let i = 0; i < 2; i++) {
+    const li = document.createElement("li");
+    li.textContent = `Item ${count++}`;
+    infinite.appendChild(li);
+  }
+}
+})
+ 
 
